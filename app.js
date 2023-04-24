4d4a48c02391202908be607f19537818984fc771
MarkMa2003 <ke.mark.ma2021@gmail.com>
2023-04-23 17:31:51 -0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const fortunes = [
-  "You will have a great day.",
-  "Success is in your future.",
-  "You will find true love soon.",
-  "A big opportunity is coming your way.",
-  "You will receive unexpected help.",
-  "A fresh start will come your way.",
-  "You will make a new friend.",
-  "You will be surrounded by happiness.",
-  "You will discover a hidden talent.",
-  "You will receive a pleasant surprise."
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Better not tell you now",
+  "Ask again later",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "Outlook not so good",
+  "My sources say no",
+  "Very doubtful"
 ];
   
   const userInfoForm = document.getElementById("user-info");
