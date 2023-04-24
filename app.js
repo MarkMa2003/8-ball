621aa25a7d3992c85e0f32898240780f10cf820c
MarkMa2003 <ke.mark.ma2021@gmail.com>
2023-04-23 17:11:50 -0700

    update

--- app.js
+++ app.js
@@ -1,51 +1,40 @@
-const fortunes = {
-    love: [
-      "You will meet someone special soon.",
-      "An old flame may rekindle.",
-      "Your love life will take a surprising turn.",
-    ],
-    career: [
-      "A great opportunity awaits you.",
-      "You will receive a promotion.",
-      "A career change may be on the horizon.",
-    ],
-    health: [
-      "You will find the strength to overcome a health challenge.",
-      "Your energy levels will improve.",
-      "Prioritize self-care for optimal health.",
-    ],
-    general: [
-      "You will find success in unexpected places.",
-      "Your patience will be rewarded.",
-      "You will overcome a difficult challenge.",
-    ],
-  };
+const fortunes = [
+  "You will have a great day.",
+  "Success is in your future.",
+  "You will find true love soon.",
+  "A big opportunity is coming your way.",
+  "You will receive unexpected help.",
+  "A fresh start will come your way.",
+  "You will make a new friend.",
+  "You will be surrounded by happiness.",
+  "You will discover a hidden talent.",
+  "You will receive a pleasant surprise."
+];
   
   const userInfoForm = document.getElementById("user-info");
   const crystalBall = document.getElementById("crystal-ball");
   const fortuneText = document.getElementById("fortune-text");
   const loader = document.getElementById("loader");
-  const nameInput = document.getElementById("name");
+  const questionInput = document.getElementById("question");
   const submitButton = userInfoForm.querySelector("button");
-  let selectedCategory = "general";
-  
+  const goBackButton = document.getElementById("go-back");
+
   function updateSubmitButtonState() {
-    if (nameInput.value.trim()) {
+    if (questionInput.value.trim()) {
       submitButton.disabled = false;
     } else {
       submitButton.disabled = true;
     }
   }
   
-  nameInput.addEventListener("input", updateSubmitButtonState);
+  questionInput.addEventListener("input", updateSubmitButtonState);
   
   userInfoForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    const name = nameInput.value.trim();
-    selectedCategory = document.getElementById("category").value;
+    const question = questionInput.value.trim();
   
-    if (!name) {
-      alert("Please enter your name.");
+    if (!question) {
+      alert("Please enter a question.");
       return;
     }
   
@@ -53,30 +42,36 @@ const fortunes = {
     userInfoForm.style.display = "none";
   });
 
-function revealFortune() {
+  goBackButton.addEventListener("click", function () {
+    location.reload(); // Reload the page
+  });
+
+  function revealFortune() {
     crystalBall.classList.add("shake");
     loader.style.display = "block";
-    
-    setTimeout(() => {
-      loader.style.display = "none";
-      crystalBall.classList.remove("shake");
-    
-      const randomIndex = Math.floor(Math.random() * fortunes[selectedCategory].length);
-      const randomFortune = fortunes[selectedCategory][randomIndex];
-      fortuneText.textContent = randomFortune;
-    
-      fortuneText.classList.add("fade-in-up");
-      fortuneText.classList.remove("visible");
-      setTimeout(() => {
-        fortuneText.classList.add("visible");
-        fortuneText.classList.remove("fade-in-up");
-      }, 500);
   
-      setTimeout(() => {
-        fortuneText.classList.remove("visible");
-      }, 5000);
-    }, 2000);
-}
+    setTimeout(() => {
+    loader.style.display = "none";
+    crystalBall.classList.remove("shake");
+
+    const randomIndex = Math.floor(Math.random() * fortunes.length);
+    const randomFortune = fortunes[randomIndex];
+    fortuneText.textContent = randomFortune;
+
+    fortuneText.classList.add("fade-in-up");
+    fortuneText.classList.remove("visible");
+    setTimeout(() => {
+      fortuneText.classList.add("visible");
+      fortuneText.classList.remove("fade-in-up");
+      goBackButton.style.display = "block"; // Show the "Go Back" button
+    }, 500);
+
+    setTimeout(() => {
+      fortuneText.classList.remove("visible");
+    }, 5000);
+  }, 2000);
+  }
+  
 
 crystalBall.addEventListener("click", revealFortune);
 crystalBall.style.pointerEvents = "none";
